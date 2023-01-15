539d487be8c363671997ba82359eabc1d61dce3a
Hélio <insign@gmail.com>
2023-01-15 04:13:56 -0300

    removed duplicated codes from exit codes consts

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,10 @@
-## 2.0.1
+## 2.1.0 - 2023-01-15
+- removed duplicated codes from exit codes consts
+## 2.0.1 - 2023-01-15
 - fixing changelog
-## 2.0.0
+## 2.0.0 - 2023-01-15
 - much better usage with constants instead enum and more decriptive doc
 
-## 1.0.0
+## 1.0.0 - 2023-01-15
 
 - Initial version.
--- lib/src/all_exit_codes_consts.dart
+++ lib/src/all_exit_codes_consts.dart
@@ -7,8 +7,8 @@ const int generalError = 1;
 /// 2 - MisuseOfShellBuiltins - The command line usage is incorrect.
 const int misuseOfShellBuiltins = 2;
 
-/// 2 - FileNotFound - The specified file could not be found.
-const int fileNotFound = 2;
+/// 20 - NotADirectory - The specified path is not a directory.
+const int notADirectory = 20;
 
 /// 64 - WrongUsage - The command line usage is incorrect.
 const int wrongUsage = 64;
@@ -31,9 +31,6 @@ const int unableToOpenOutputFileForWriting = 70;
 /// 71 - UnableToOpenOutputFileForReading - The specified output file could not be opened for reading.
 const int unableToOpenOutputFileForReading = 71;
 
-/// 20 - NotADirectory - The specified path is not a directory.
-const int notADirectory = 20;
-
 /// 74 - IOError - An input/output error occurred.
 const int ioError = 74;
 
@@ -43,9 +40,6 @@ const int tryAgain = 75;
 /// 78 - ConfigurationError - A configuration error occurred.
 const int configurationError = 78;
 
-/// 126 - PermissionDenied - Permission to access the specified file or directory was denied.
-const int permissionDenied = 126;
-
 /// 126 - CantExecute - The command invoked cannot execute.
 const int cantExecute = 126;
 
@@ -54,9 +48,6 @@ const int notFound = 127;
 
 /// 128 - InvalidArgumentToExit - An invalid argument was passed to the exit command.
 const int invalidArgumentToExit = 128;
-
-/// 130 - Interrupted - The operation was interrupted.
-const int interrupted = 130;
 
 /// 130 - UserTerminated - The script was terminated by the user.
 const int userTerminated = 130;
